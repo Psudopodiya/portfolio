c7632dea6e3c51cfe68a9f82501fadbbc97d2b5a
Psudopodiya <chinmay.raiker2002@gmail.com>
2024-12-15 14:26:29 +0530

    updated resume

--- src/utils/data.ts
+++ src/utils/data.ts
@@ -30,13 +30,13 @@ export const projects: Project[] = [
       "JavaScript",
       "Django Channels",
     ],
-    info: "This project is a real-time chat application that combines a robust Django Channels and PostgreSQL backend with a modern React, Tailwind CSS, ShadCN, TypeScript, and Vite frontend. It supports one-on-one and group chats with seamless real-time communication, user notifications, and an admin interface for managing users and chat rooms. The frontend offers a clean, responsive design with fast performance and real-time updates, making the platform ideal for efficient and engaging communication.",
+    info: "This project is a real-time chat application featuring a Django Channels and PostgreSQL backend alongside a modern React, Tailwind CSS, ShadCN, TypeScript, and Vite frontend. It supports one-on-one and group chats, real-time updates, user notifications, and admin tools for managing users and chat rooms. The responsive frontend delivers a clean design with fast performance, enabling seamless and engaging communication.",
     link: "https://github.com/Psudopodiya/Retro-Chat",
     repository_link: "https://github.com/Psudopodiya/Retro-Chat",
   },
 
   {
-    name: "Retro Chat",
+    name: "3js Landing Page",
     tech: [
       "React",
       "Tailwind",
@@ -46,7 +46,7 @@ export const projects: Project[] = [
       "Zustand",
       "TypeScript",
     ],
-    info: "This project is a demonstration for the 3js website that functions and can be used as a landing page for other websites, serving as an interactive way for users to use them an guide around ",
+    info: "This project is a demonstration for a 3Js website that functions, and can be used as a landing page for other websites, serving as an interactive way for users to use them an guide around. Furthere more this has been used in internal Projects like ",
     link: "https://landing-page-gamma-liard-58.vercel.app/",
     repository_link: "https://github.com/Psudopodiya/3js-COE",
   },
